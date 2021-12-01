44b98b904b49e9b82e79053de2a2a17eb9cc2570
chand <chand@DESKTOP-NB9NGI7>
2021-12-01 06:15:12 -0500

    changes

--- src/conflict/PracticeConflict.java
+++ src/conflict/PracticeConflict.java
@@ -4,11 +4,7 @@ public class PracticeConflict {
 	
 	public static void main(String[] args) {
 		System.out.println("creating conflict");
-
-
-		
-		
-		System.out.println("pushing without stashing deleting");
+      System.out.println("pushing without stashing deleting");
 
 		
 		
