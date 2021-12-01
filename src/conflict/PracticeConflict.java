80ae58e9aa8982e047cf120f05458bddf3bb71c7
sara1chand <94859370+sara1chand@users.noreply.github.com>
2021-12-01 06:13:50 -0500

    Update PracticeConflict.java
    deleting things

--- src/conflict/PracticeConflict.java
+++ src/conflict/PracticeConflict.java
@@ -5,11 +5,8 @@ public class PracticeConflict {
 	public static void main(String[] args) {
 		System.out.println("creating conflict");
 
-		System.out.println("lets try this. Editing");
+
 		
-		System.out.println("make change agian");
-		
-		System.out.println("make change agian");
 		
 		System.out.println("pushing without stashing deleting");
 
