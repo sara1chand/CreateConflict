9633fbaad93b81b1355c68486f9c0e235107601d
sara1chand <94859370+sara1chand@users.noreply.github.com>
2021-12-01 05:51:46 -0500

    Update PracticeConflict.java
    creating c,onflict

--- src/conflict/PracticeConflict.java
+++ src/conflict/PracticeConflict.java
@@ -3,7 +3,7 @@ package conflict;
 public class PracticeConflict {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("creating conflict");
 		System.out.println("lets try this");
 	}
 
