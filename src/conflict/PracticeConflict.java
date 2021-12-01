ee4bbabc836b3a060b9be516ce2c9ec1274a2de4
sara1chand <94859370+sara1chand@users.noreply.github.com>
2021-12-01 06:16:25 -0500

    Update PracticeConflict.java
    aadded

--- src/conflict/PracticeConflict.java
+++ src/conflict/PracticeConflict.java
@@ -3,6 +3,8 @@ package conflict;
 public class PracticeConflict {
 	
 	public static void main(String[] args) {
+		
+	System.out.println("adding");
 		System.out.println("creating conflict");
       System.out.println("pushing without stashing deleting");
 
